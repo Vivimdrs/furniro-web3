15ef189decd0812b6835c7b72a8fcae892e8ade5
Eros Franklin <erosfranklinfelix2016@gmail.com>
2026-08-03 15:03:58 -0300

    feat: add routes for products

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { productRoutes } from "./routes/products.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/products', productRoutes);
+
+app.use(errorMiddleware);
 
 export default app;
