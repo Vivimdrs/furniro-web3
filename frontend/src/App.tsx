37ec01e47d99f83620b5a290f351bcf2303698b9
Vitoria Medeiros <128424701+Vivimdrs@users.noreply.github.com>
2026-08-25 15:08:59 -0300

    Revert "Developer"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,39 +3,30 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import Home from "./pages/Home/page";
 import Footer from "./components/Footer";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Product from "./pages/Product/page";
 import Shop from "./pages/Shop/page";
 import Cart from "./pages/Cart/page";
 import NotFoundPage from "./pages/NotFoundPage";
-import SignUp from "./pages/Auth/Singup";
-import Login from "./pages/Auth/Login";
 
 const App = () => {
-    const location = useLocation();
-    const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
-
     return (
         <>
             <Toaster />
-            {!isAuthPage && (
-                <Container className="bg-[#FFF]">
-                    <Header />
-                </Container>
-            )}
+
+            <Container className="bg-[#FFF]">
+                <Header />
+            </Container>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/shop/:category?" element={<Shop />} />
                 <Route path="/product/:slug" element={<Product />} />
                 <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
-            {!isAuthPage && (
-                <Container className="bg-primary border-t border-t-[rgba(0,0,0,0.17)]">
-                    <Footer />
-                </Container>
-            )}
+            <Container className="bg-primary border-t border-t-[rgba(0,0,0,0.17)]">
+                <Footer />
+            </Container>
         </>
     );
 };
